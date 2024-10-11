66f156fd1e276520302fc91a41391edf06798b43
Erik-42 <mesen.erik@gmail.com>
2024-10-11 19:34:19 +0200

    Ajout de la récupération de l'IP utilisateur dans le nom root

--- src/components/userInput/userInput.jsx
+++ src/components/userInput/userInput.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import "./__userInput.scss";
 
 // Import des composants React correspondant aux commandes
@@ -14,6 +14,35 @@ import Credits from "../credits/credits";
 
 export default function UserInput() {
 	const [activeCommand, setActiveCommand] = useState(null);
+	const [rootValue, setRootValue] = useState("root@Erik_Mesen's Website:~$ ");
+	const spanRef = useRef(null);
+	const inputRootRef = useRef(null);
+
+	// Fonction pour ajuster la largeur de l'input en fonction du contenu
+	useEffect(() => {
+		if (spanRef.current && inputRootRef.current) {
+			// Mettre à jour la largeur de l'input en fonction de la largeur du span
+			inputRootRef.current.style.width = `${spanRef.current.offsetWidth}px`;
+		}
+	}, [rootValue]);
+
+	// Récupérer l'adresse IP de l'utilisateur
+	useEffect(() => {
+		const fetchIPAddress = async () => {
+			try {
+				const response = await fetch("https://api.ipify.org?format=json");
+				const data = await response.json();
+				setRootValue(`root@${data.ip}:~$ `);
+			} catch (error) {
+				// Si l'IP n'est pas disponible, je laisse la valeur par défaut
+				console.error("Impossible de récupérer l'adresse IP:", error);
+			}
+		};
+
+		// Essaye d'obtenir le nom du PC (ce qui n'est pas possible côté client).
+		// Récupération direct de l'adresse IP à défaut.
+		fetchIPAddress();
+	}, []);
 
 	const commandProcessor = (e) => {
 		if (e.key === "Enter") {
@@ -50,15 +79,17 @@ export default function UserInput() {
 	return (
 		<div className='userInput'>
 			<div className='userInput__output'>{activeCommand}</div>
-			{/* <label htmlFor="txtBox" className='userInput__label'>
-        Entrer une commande
-      </label> */}
 			<div className='userInput__wrapper'>
+				{/* Span caché pour mesurer la taille du texte */}
+				<span ref={spanRef} className='userInput__hiddenSpan'>
+					{rootValue}
+				</span>
 				<input
 					type='text'
-					value="root@Erik_Mesen's Website:~$ "
+					value={rootValue}
 					readOnly
 					className='userInput__root'
+					ref={inputRootRef} // Référence pour ajuster la largeur
 				/>
 				<input
 					type='text'
