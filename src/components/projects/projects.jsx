3d2244b5b8d92fc53dad6eec5da53a06f7d0ba38
Erik-42 <mesen.erik@gmail.com>
2024-10-18 07:58:28 +0200

    Mise à jour des projects

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "./__projects.scss";
-import projetPerso1 from "../../assets/img/Projets-perso/Aerial-by-HTML5-UP.png";
-import projetPerso2 from "../../assets/img/Projets-perso/CetD_2023.webp";
-import projetPerso3 from "../../assets/img/Projets-perso/Le-Raid-Tolkien.webp";
-import projetPro1 from "../../assets/img/Projets-pro/Banner-Ohmyfood.png";
-import projetPro2 from "../../assets/img/Projets-pro/argentBankLogo.png";
-import projetPro3 from "../../assets/img/Projets-pro/Kasa-mini.webp";
+import sportSee from "../../assets/img/Projets-pro/logoSportSee.png";
+import argentBank from "../../assets/img/Projets-pro/argentBankLogo.png";
+import wealthHealth from "../../assets/img/Projets-pro/logo-wealthhealth-nobackground.svg";
+import cEtD from "../../assets/img/Projets-perso/CetD_2023.webp";
+import raidTolkien from "../../assets/img/Projets-perso/Le-Raid-Tolkien.webp";
+import bashPortfolio from "../../assets/img/Projets-perso/Erik-Mesen-portfolio.png";
 
 export function ResumeProjects() {
 	return (
@@ -17,51 +17,66 @@ export function ResumeProjects() {
 
 					<div className='resumeProjects__wrapper__professional-1 resumeProjects__item'>
 						<h4 className='resumeProjects__wrapper__professional__title'>
-							project Title 1
+							ArgentBank
 						</h4>
 						<div className='resumeProjects__wrapper__professional__container'>
 							<p className='resumeProjects__wrapper__professional__container__text'>
-								Une application de gestion de tâches professionnelle utilisant
-								React et Node.js.
+								Utilisez une API pour un compte utilisateur bancaire avec React
+								<br />
+								https://github.com/Erik-42/openclassroom-projet_13-argentbank
+								<br />
+								Site développé dans le cadre de ma formation "Développeur
+								d'applications JavaScript React"
 							</p>
-							<img
+							<a
 								className='resumeProjects__wrapper__professional__container__img'
-								src={projetPro1}
-								alt={projetPro1}
-							/>
+								href='https://github.com/Erik-42/openclassroom-projet_13-argentbank'
+								_blank>
+								<img src={argentBank} alt={argentBank} />
+							</a>
 						</div>
 					</div>
 
 					<div className='resumeProjects__wrapper__professional-2 resumeProjects__item'>
 						<h4 className='resumeProjects__wrapper__professional__title'>
-							project Title 2
+							SportSee
 						</h4>
 						<div className='resumeProjects__wrapper__professional__container'>
 							<p className='resumeProjects__wrapper__professional__container__text'>
-								Une application de gestion de tâches professionnelle utilisant
-								React et Node.js.
+								Développez un tableau de bord d'analytics avec React
+								<br />
+								https://github.com/Erik-42/openclassroom-projet_12-sportsee
+								<br />
+								Site développé dans le cadre de ma formation "Développeur
+								d'applications JavaScript React"
 							</p>
-							<img
+							<a
 								className='resumeProjects__wrapper__professional__container__img'
-								src={projetPro2}
-								alt={projetPro2}
-							/>
+								href='https://github.com/Erik-42/openclassroom-projet_12-sportsee'>
+								<img src={sportSee} alt={sportSee} />
+							</a>
 						</div>
 					</div>
 					<div className='resumeProjects__wrapper__professional-3 resumeProjects__item'>
 						<h4 className='resumeProjects__wrapper__professional__title'>
-							project Title 3
+							wealthHealth
 						</h4>
 						<div className='resumeProjects__wrapper__professional__container'>
 							<p className='resumeProjects__wrapper__professional__container__text'>
-								Une application de gestion de tâches professionnelle utilisant
-								React et Node.js.
+								Faites passer une librairie jQuery vers React
+								<br />
+								https://github.com/Erik-42/openclassroom-projet_14-wealthhealth
+								<br />
+								https://github.com/Erik-42/openclassroom-projet_14-wealthhealth-npm
+								<br />
+								Site développé dans le cadre de ma formation "Développeur
+								d'applications JavaScript React"
 							</p>
-							<img
+							<a
 								className='resumeProjects__wrapper__professional__container__img'
-								src={projetPro3}
-								alt={projetPro3}
-							/>
+								href='https://github.com/Erik-42/openclassroom-projet_14-wealthhealth'>
+								<img src={wealthHealth} alt={wealthHealth} />
+							</a>
 						</div>
 					</div>
 				</div>
@@ -71,53 +86,62 @@ export function ResumeProjects() {
 
 					<div className='resumeProjects__wrapper__personal-1 resumeProjects__item'>
 						<h4 className='resumeProjects__wrapper__personal__title'>
-							project Title 1
+							Cidre & Dragon
 						</h4>
 						<div className='resumeProjects__wrapper__personal__container'>
 							<p className='resumeProjects__wrapper__personal__container__text'>
-								Une application de gestion de tâches personnelle utilisant React
-								et Node.js.
+								Site du festival Cidre & Dragon
+								<br />
+								Site développé sous Joomla
+								<br />
+								https://github.com/Erik-42/CidreEtDragon
 							</p>
-							<a href=''>
-								<img
-									className='resumeProjects__wrapper__personal__container__img'
-									src={projetPerso1}
-									alt={projetPerso1}
-								/>
+							<a
+								className='resumeProjects__wrapper__professional__container__img'
+								href='https://github.com/Erik-42/CidreEtDragon'>
+								<img src={cEtD} alt={cEtD} />
 							</a>
 						</div>
 					</div>
 
 					<div className='resumeProjects__wrapper__personal-2 resumeProjects__item'>
 						<h4 className='resumeProjects__wrapper__personal__title'>
-							project Title 2
+							Le Raid Tolkien
 						</h4>
 						<div className='resumeProjects__wrapper__personal__container'>
 							<p className='resumeProjects__wrapper__personal__container__text'>
-								Une application de gestion de tâches personnelle utilisant React
-								et Node.js.
+								Site de l'association "Le Raid Tolkien"
+								<br />
+								Site développé en React Redux
+								<br />
+								https://github.com/Erik-42/raidtolkien
 							</p>
-							<img
-								className='resumeProjects__wrapper__personal__container__img'
-								src={projetPerso2}
-								alt={projetPerso2}
-							/>
+							<a
+								className='resumeProjects__wrapper__professional__container__img'
+								href='https://github.com/Erik-42/raidtolkien'>
+								<img src={raidTolkien} alt={raidTolkien} />
+							</a>
 						</div>
 					</div>
 					<div className='resumeProjects__wrapper__personal-3 resumeProjects__item'>
 						<h4 className='resumeProjects__wrapper__personal__title'>
-							project Title 3
+							Bash Portfolio
 						</h4>
 						<div className='resumeProjects__wrapper__personal__container'>
 							<p className='resumeProjects__wrapper__personal__container__text'>
-								Une application de gestion de tâches personnelle utilisant React
-								et Node.js.
+								Mon portfolio
+								<br />
+								Site développé en React Redux sur la base du site
+								<br />
+								<a href='#'>Bash-CV</a>
+								<br />
+								https://github.com/Erik-42/bash-cv
 							</p>
-							<img
-								className='resumeProjects__wrapper__personal__container__img'
-								src={projetPerso3}
-								alt={projetPerso3}
-							/>
+							<a
+								className='resumeProjects__wrapper__professional__container__img'
+								href='https://github.com/Erik-42/bash-cv'>
+								<img src={bashPortfolio} alt={bashPortfolio} />
+							</a>
 						</div>
 					</div>
 				</div>
@@ -131,6 +155,41 @@ export default function Projects() {
 		<section className='projects'>
 			<h2 className='projects__title'>Projects:</h2>
 			<div className='projects__list'>
+				<div className='projects__list__professional'>
+					<div className='projects__list__professional-1 project__item'>
+						<h4 className='projects__list__professional__title'>
+							Projet personnel 1:
+						</h4>
+						<p className='projects__list__professional__description'>
+							Une application de gestion de tâches personnelle utilisant React
+							et Node.js.
+						</p>
+						<img className='projects__list__professional__img' src='' alt='' />
+					</div>
+
+					<div className='projects__list__professional-2 project__item'>
+						<h4 className='projects__list__professional__title'>
+							Projet personnel 2:
+						</h4>
+						<p className='projects__list__professional__description'>
+							Une application de gestion de tâches personnelle utilisant React
+							et Node.js.
+						</p>
+						<img className='projects__list__professional__img' src='' alt='' />
+					</div>
+
+					<div className='projects__list__professional-3 project__item'>
+						<h4 className='projects__list__professional__title'>
+							Projet personnel 3:
+						</h4>
+						<p className='projects__list__professional__description'>
+							Une application de gestion de tâches personnelle utilisant React
+							et Node.js.
+						</p>
+						<img className='projects__list__professional__img' src='' alt='' />
+					</div>
+				</div>
+
 				<div className='projects__list__personal'>
 					<h3>Projets Personnel:</h3>
 
@@ -175,41 +234,6 @@ export default function Projects() {
 							et Node.js.
 						</p>
 						<img className='projects__list__personal__img' src='' alt='' />
-					</div>
-				</div>
-
-				<div className='projects__list__professional'>
-					<div className='projects__list__professional-1 project__item'>
-						<h4 className='projects__list__professional__title'>
-							Projet personnel 1:
-						</h4>
-						<p className='projects__list__professional__description'>
-							Une application de gestion de tâches personnelle utilisant React
-							et Node.js.
-						</p>
-						<img className='projects__list__professional__img' src='' alt='' />
-					</div>
-
-					<div className='projects__list__professional-2 project__item'>
-						<h4 className='projects__list__professional__title'>
-							Projet personnel 2:
-						</h4>
-						<p className='projects__list__professional__description'>
-							Une application de gestion de tâches personnelle utilisant React
-							et Node.js.
-						</p>
-						<img className='projects__list__professional__img' src='' alt='' />
-					</div>
-
-					<div className='projects__list__professional-3 project__item'>
-						<h4 className='projects__list__professional__title'>
-							Projet personnel 3:
-						</h4>
-						<p className='projects__list__professional__description'>
-							Une application de gestion de tâches personnelle utilisant React
-							et Node.js.
-						</p>
-						<img className='projects__list__professional__img' src='' alt='' />
 					</div>
 				</div>
 			</div>
